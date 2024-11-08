aa8caaf09146f1e85b178f3df94275fe380e7fd2
Tripp-dripp <82460275+Tripp-dripp@users.noreply.github.com>
2024-11-07 20:08:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -75,17 +75,10 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on("cookieClicked", (multiplier = 1) => {
+    socket.on("cookieClicked", () => {
         if (players[socket.id] && gameStarted) {
-            players[socket.id].score += multiplier;
+            players[socket.id].score += 1;
             players[socket.id].cookieSize = Math.min(300, players[socket.id].cookieSize + 5);
-            io.emit("updatePlayers", players);
-        }
-    });
-
-    socket.on("updateScore", (newScore) => {
-        if (players[socket.id] && gameStarted) {
-            players[socket.id].score = newScore;
             io.emit("updatePlayers", players);
         }
     });
